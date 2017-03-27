5e9be54ab9b075f43c3642c8f8283bd562ef671a
Troels E. Linnet <tlinnet@gmail.com>
2017-03-27 16:14:45 +0200

    Added more packages

--- deploy_google_computing_ubuntu_16-04_86_x64_manual.sh
+++ deploy_google_computing_ubuntu_16-04_86_x64_manual.sh
@@ -5,14 +5,20 @@
 # Guide taken from:
 # https://www.mediawiki.org/wiki/Manual:Running_MediaWiki_on_Debian_or_Ubuntu
 # https://help.ubuntu.com/community/MediaWiki
+# https://hostpresto.com/community/tutorials/how-to-install-and-configure-mediawiki-on-ubuntu-16-04/
 
 # Install apt-get packages
 function doaptget {
     sudo apt-get -y update
     sudo apt-get -y upgrade
 
+    # Install lamp
+    sudo apt-get -y install tasksel
+    sudo tasksel install lamp-server
+
     # Install LAMP server
     sudo apt-get -y install apache2 mysql-server php php-mysql libapache2-mod-php php-xml php-mbstring
+    sudo apt-get -y install php-curl php-json php-cgi
 
     # Additional
     #Alternative PHP Cache	php-apcu or php5-apcu	Modern MediaWiki versions will automatically take advantage of this being installed for improved performance.
@@ -21,17 +27,22 @@ function doaptget {
     # PHP Unicode normalization	php-intl or php5-intl	MediaWiki will fallback to a slower PHP implementation if not available.
     sudo apt-get -y install php-intl
 
-    # ImageMagick	imagemagick	Image thumbnailing.
-    sudo apt-get -y install imagemagick
-
-    # Inkscape	inkscape	Alternative means of SVG thumbnailing, than ImageMagick. Sometimes it will render SVGs better if originally created in Inkscape.
-    sudo apt-get -y install inkscape
-
     # PHP GD library	php-gd or php5-gd	Alternative to ImageMagick for image thumbnailing.
     sudo apt-get -y install php-gd
 
     # PHP command-line	php-cli or php5-cli	Ability to run PHP commands from the command line, which is useful for debugging and running maintenance scripts.
     sudo apt-get -y install php-cli
+
+    # MediaWiki can be configured to send email messages for various functions. You will need to install some additional packages:
+    sudo apt-get -y install php-pear
+    sudo pear install mail
+    sudo pear install Net_SMTP
+
+    # ImageMagick	imagemagick	Image thumbnailing.
+    sudo apt-get -y install imagemagick
+
+    # Inkscape	inkscape	Alternative means of SVG thumbnailing, than ImageMagick. Sometimes it will render SVGs better if originally created in Inkscape.
+    sudo apt-get -y install inkscape
 
     # Install lynx
     sudo apt-get -y install lynx
@@ -44,7 +55,7 @@ function dogetversion {
     echo "Current version of wikimedia is: $WIKIV"
 }
 
-# Get latest compiled version of relax
+# Get latest compiled version of wikimedia
 function dogetwikimedia {
     mkdir -p "$HOME/Downloads"
     cd "$HOME/Downloads"
@@ -55,13 +66,13 @@ function dogetwikimedia {
     fi
 
     # Move the files
-    if [ ! -d "/var/lib/mediawiki" ]; then
-        sudo mkdir -p /var/lib/mediawi
-        sudo mv "$HOME/Downloads/mediawiki-$WIKIV" "/var/lib/mediawiki"
+    if [ ! -d "/var/www/html/mediawiki" ]; then
+        sudo mkdir -p /var/www/html/mediawi
+        sudo mv "$HOME/Downloads/mediawiki-$WIKIV" "/var/www/html/mediawiki"
     fi
 
-    # Make a link
-    sudo ln -s /var/lib/mediawiki /var/www/html/mediawiki
+    #Change the ownership of mediawiki directory to www-data:
+    sudo www-data:www-data -R /var/www/html/mediawiki
 }
 
 # Change apache settings
@@ -81,5 +92,7 @@ function doinstall {
     dogetversion
     dogetwikimedia
     doapache
+
+    echo "Please visit for installation: http://SERVER-IP/mediawiki"
 }
 
