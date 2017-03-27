1680a59f4f94c6774fd0b2724fc8ecbbeef4a4aa
Troels E. Linnet <tlinnet@gmail.com>
2017-03-27 13:46:53 +0200

    Added additional packages

--- deploy_google_computing_ubuntu_16-04_86_x64.sh
+++ deploy_google_computing_ubuntu_16-04_86_x64.sh
@@ -6,4 +6,26 @@
 function doaptget {
     sudo apt-get -y update
     sudo apt-get -y upgrade
+
+    # Install LAMP server
+    sudo apt-get -y install apache2 mysql-server php php-mysql libapache2-mod-php php-xml php-mbstring
+
+    # Additional
+    #Alternative PHP Cache	php-apcu or php5-apcu	Modern MediaWiki versions will automatically take advantage of this being installed for improved performance.
+    sudo apt-get -y install php-apcu
+
+    # PHP Unicode normalization	php-intl or php5-intl	MediaWiki will fallback to a slower PHP implementation if not available.
+    sudo apt-get -y install php-intl
+
+    # ImageMagick	imagemagick	Image thumbnailing.
+    sudo apt-get -y install imagemagick
+
+    # Inkscape	inkscape	Alternative means of SVG thumbnailing, than ImageMagick. Sometimes it will render SVGs better if originally created in Inkscape.
+    sudo apt-get -y install inkscape
+
+    # PHP GD library	php-gd or php5-gd	Alternative to ImageMagick for image thumbnailing.
+    sudo apt-get -y install php-gd
+
+    # PHP command-line	php-cli or php5-cli	Ability to run PHP commands from the command line, which is useful for debugging and running maintenance scripts.
+    sudo apt-get -y install php-cli
 }
