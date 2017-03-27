ef7c355105dc5aca62d6a5dc3b61e7b31a68acd8
Troels E. Linnet <tlinnet@gmail.com>
2017-03-27 14:11:49 +0200

    Added function to get latest wikimedia

--- deploy_google_computing_ubuntu_16-04_86_x64.sh
+++ deploy_google_computing_ubuntu_16-04_86_x64.sh
@@ -28,4 +28,38 @@ function doaptget {
 
     # PHP command-line	php-cli or php5-cli	Ability to run PHP commands from the command line, which is useful for debugging and running maintenance scripts.
     sudo apt-get -y install php-cli
+
+    # Install lynx
+    sudo apt-get -y install lynx
+}
+
+function dogetversion {
+    # From the wiki, get current versions
+    WIKIV=`lynx -dump "https://www.mediawiki.org/wiki/Download" | grep "Download MediaWiki" | head -n 1 | cut -d"]" -f 3 | cut -d" " -f3`
+    WIKIVT=`echo $WIKIV | cut -d"." -f1-2`
+
+    echo "Current version of wikimedia is: $WIKIV"
+}
+
+
+# Get latest compiled version of relax
+function dogetwikimedia {
+    mkdir -p "$HOME/Downloads"
+    cd "$HOME/Downloads"
+    if [ ! -d "$HOME/Downloads/mediawiki-$WIKIV" ]; then
+        curl https://releases.wikimedia.org/mediawiki/$WIKIVT/mediawiki-$WIKIV.tar.gz -o mediawiki-$WIKIV.tar.gz
+        tar -xvzf mediawiki-$WIKIV.tar.gz
+        rm mediawiki-$WIKIV.tar.gz
+    fi
+
+    if [ ! -d "/var/lib/mediawiki" ]; then
+        sudo mkdir -p /var/lib/mediawi
+        sudo mv "$HOME/Downloads/mediawiki-$WIKIV" "/var/lib/mediawiki"
+    fi
+}
+
+# Combine functions
+function doinstall {
+    dogetversion
+    dogetwikimedia
 }
