bd12a12d2e3f65433caca9b95fdd5f5168ef80f4
Troels E. Linnet <tlinnet@gmail.com>
2017-03-27 13:32:58 +0200

    Added initial apt-get command

--- deploy_google_computing_ubuntu_16-04_86_x64.sh
+++ deploy_google_computing_ubuntu_16-04_86_x64.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 # -*- coding: UTF-8 -*-
 # Script for deploying wikimedia on Google Cloud Computing GCC
+
+# Install apt-get packages
+function doaptget {
+    sudo apt-get update
+    sudo apt-get upgrade
+}
